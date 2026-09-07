0062e4199cfd2218043e88eb5d6b001497fa98e4
Dronedale <Dronedale@users.noreply.github.com>
2026-09-07 20:45:35 +0200

    0.2.0

--- custom_components/ha_intercom/entity.py
+++ custom_components/ha_intercom/entity.py
@@ -9,7 +9,7 @@ from homeassistant.helpers.entity import Entity
 from .const import DOMAIN
 from .manager import IntercomManager
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 
 # Englische Namen fuer die Objekt-IDs, damit die Entitaets-IDs in jeder Sprache gleich lauten
 # (z. B. number.intercom_ring_duration). Die Anzeigenamen kommen aus den Uebersetzungen.
